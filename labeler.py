1a5703ef5627bc9becf218cee654097e5f029e4c
Jackson <jrmoffet@gmail.com>
2020-02-12 17:05:51 -0500

    udpate labeler

--- labeler.py
+++ labeler.py
@@ -1,85 +1,72 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#!/usr/bin/env python3
+import cv2
 import sys
 import os
 from pathlib import Path
+import argparse
+import shutil
 
 
 class Labeler:
-    def __init__(self, img_directory, classes, dataset_dir=""):
+    def __init__(self, img_dir, classes, output_dir):
         """
 
-        :param img_directory:  The directory containing the images to label
-        :type str
-        :param classes: The classes to label the images with
-        :type list
-        :param dataset_dir: (Optional) The directory to move the images to, containing folders for each class
-        :type str
-        """
-        self.img_directory = img_directory
-        self.dataset_dir = dataset_dir
+              :param img_dir:  The directory containing the images to label
+              :type str
+              :param classes: The classes to label the images with
+              :type list
+              :param output_dir: (Optional) The directory to move the images to, containing folders for each class
+              :type str
+              """
+        self.img_directory = img_dir
+        self.dataset_dir = output_dir
         self.classes = classes
         print(f"Classes are {self.classes}")
-        if not os.path.exists(img_directory):
+        if not os.path.exists(self.img_directory):
             raise Exception("Invalid img_directory")
         img_types = ["*.jpg", "*.png"]
         # Get the paths of all of the images in the img_directory that have an extension in img_types
-        self.images = [img for img_ in [Path(img_directory).rglob(t) for t in img_types] for img in img_]
+        self.image_paths = [img for img_ in [Path(self.img_directory).rglob(t) for t in img_types] for img in img_]
         self.labels = {}
         self.img_idx = 0
         self.label_idx = None
 
     def label(self):
-        for image in self.images:
-            fig, ax = plt.subplots()
-            fig.canvas.mpl_connect('key_press_event', self.press_handler)
-            print(f"Opening {image}")
-            img = mpimg.imread(str(image))
-            ax.imshow(img)
-            # This only supports 10 classes for now
-            label_opts = ", ".join([f"{i}: {label}" for i, label in enumerate(self.classes)])
-            plt.title(label_opts)
-            plt.show()
-        plt.close(fig)
-        self.write_labels()
+        label_opts = {}
+        for i, label in enumerate(self.classes):
+            label_opts[i + 1] = label
+            folder_path = os.path.join(self.dataset_dir, label)
+            try:
+                os.mkdir(folder_path)
+            except FileExistsError:
+                pass
+        valid_keys = [str(i + 1) for i in range(len(self.classes))]
+        valid_keys.append('d')
+        for image_path in self.image_paths:
+            img = cv2.imread(str(image_path))
+            cv2.imshow(str(label_opts), img)
+            key = chr(cv2.waitKey(0))
+            while key not in valid_keys:
+                print(f"Press a valid key: {label_opts}, d")
+                key = chr(cv2.waitKey(0))
+            if key == 'd':
+                print(f"You dropped {image_path.name}")
+                continue
+            key = int(key)
+            file_name = image_path.name
+            print(f"You labeled {image_path.name} as {label_opts[key]}")
+            destination = os.path.join(self.dataset_dir, label_opts[key], file_name)
 
-    def write_labels(self):
-        print("writing labels")
-        if not self.dataset_dir == "":
-            self.move_files()
-        label_dir = self.dataset_dir if not self.dataset_dir == "" else self.img_directory
-        with open(os.path.join(label_dir, "labels.csv"), "w") as label_file:
-            label_file.write("file,label\n")
-            for label in self.labels.keys():
-                for file in self.labels[label]:
-                    label_file.write(f"{file},{label}\n")
-        print("Labels written")
-
-    def move_files(self):
-        if not os.path.exists(self.dataset_dir):
-            os.mkdir(self.dataset_dir)
-        for c in self.labels.keys():
-            if not os.path.exists(os.path.join(self.dataset_dir, c)):
-                os.mkdir(os.path.join(self.dataset_dir, c))
-            for i, file in enumerate(self.labels[c]):
-                filename = file.split('/')[-1]
-                dest = os.path.join(self.dataset_dir, c, filename)
-                self.labels[c][i] = dest
-                print(f"Moving {file} to {dest}")
-                os.rename(file, dest)
-
-    def press_handler(self, event):
-        self.label_idx = int(event.key)
-        img_label = self.classes[self.label_idx]
-        print(f"You labeled {self.images[self.img_idx]} as {img_label}")
-        plt.close()
-        if img_label not in self.labels:
-            self.labels[img_label] = []
-        self.labels[img_label].append(str(self.images[self.img_idx]))
-        sys.stdout.flush()
-        self.img_idx += 1
+            print(f"Move {image_path} to {destination}")
+            shutil.copyfile(str(image_path), destination)
 
 
 if __name__ == "__main__":
-    labeler = Labeler("./data", ["apple", "not apple"], dataset_dir="def_not_data")
+    parser = argparse.ArgumentParser(description="A script for labeling recognition data")
+    parser.add_argument('--input_dir', '-i', default='./data')
+    parser.add_argument('--output_dir', '-o', default='./labeled_data')
+    parser.add_argument('--labels', '-l', nargs='+', help='<REQUIRED> The labels for data', required=True)
+    args = parser.parse_args()
+
+    labeler = Labeler(args.input_dir, args.labels, args.output_dir)
     labeler.label()
